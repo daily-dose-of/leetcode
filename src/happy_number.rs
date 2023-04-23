c2edfae5cc2b85d7f75449d9b098777054303d4d
wiseaidev <business@wiseai.dev>
2023-04-23 18:16:39 +0300

    add solution to problem `202. Happy Number`

---
+++ src/happy_number.rs
@@ -0,0 +1,65 @@
+pub struct HappyNumberSolution {}
+
+impl HappyNumberSolution {
+    /// Determines whether a number is happy.
+    ///
+    /// A happy number is a number defined by the following process:
+    /// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+    /// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a 
+    /// cycle which does not include 1.
+    /// Those numbers for which this process ends in 1 are happy.
+    ///
+    /// # Arguments
+    /// * `n` - A positive integer to check for happiness
+    ///
+    /// # Returns
+    /// (`bool`): `true` if the input number is happy, `false` otherwise
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use leet_rs::happy_number::HappyNumberSolution;
+    ///
+    /// let is_happy = HappyNumberSolution::is_happy(19);
+    /// assert_eq!(is_happy, true);
+    ///
+    /// let is_happy = HappyNumberSolution::is_happy(2);
+    /// assert_eq!(is_happy, false);
+    /// ```
+    pub fn is_happy(n: i32) -> bool {
+        let mut n = n;
+        let mut set = std::collections::HashSet::new();
+        while n != 1 && !set.contains(&n) {
+            set.insert(n);
+            n = Self::square_sum(n);
+        }
+        n == 1
+    }
+
+    /// Computes the sum of the squares of the digits of a given number.
+    ///
+    /// # Arguments
+    ///
+    /// * `n` - A positive integer whose digits should be squared and summed
+    ///
+    /// # Returns
+    ///The sum of the squares of the digits of `n`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use leet_rs::happy_number::HappyNumberSolution;
+    ///
+    /// let square_num = HappyNumberSolution::square_sum(19);
+    /// assert_eq!(square_num, 82);
+    /// ```
+    pub fn square_sum(mut n: i32) -> i32 {
+        let mut sum = 0;
+        while n > 0 {
+            let digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        sum
+    }
+}
